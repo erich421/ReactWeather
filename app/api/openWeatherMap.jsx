cf06d12ddf19f3e6556a34676a70375bac334e03
Eric Hendershott <erich421@comcast.net>
2016-08-07 22:34:16 -0500

    Add nav search support

--- app/api/openWeatherMap.jsx
+++ app/api/openWeatherMap.jsx
@@ -1,6 +1,6 @@
 var axios = require('axios');
 
-const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=c4e735ea8bd7e7b6dc8368c752517b2d&units=imperial';
+const OPEN_WEATHER_MAP_URL = 'http://api.openweathermap.org/data/2.5/weather?appid=b1841ae34cdf15aaf3a93e650dedc282&units=imperial';
 
 module.exports = {
   getTemp: function (location) {
